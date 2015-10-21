c37af0bc9250f67344c3c5f9e95b8011d6e9916f
Michael Huang <mikhuang@gmail.com>
2015-10-21 16:24:18 -0700

    clean up import parse json

--- parsesync/management/commands/importparsejson.py
+++ parsesync/management/commands/importparsejson.py
@@ -12,17 +12,25 @@ from parsesync.models import ParseModel
 from urllib import urlretrieve
 import os
 
+"""
+
+Example: ./manage.py importparsejson /Users/username/some_export/_User.json --model=CustomUser
+
+"""
+
 class Command(BaseCommand):
     help = 'Sync exported data from Parse to Django. Expects path to file as argument'
 
     # Django < 1.7- support
     option_list = BaseCommand.option_list + (
         make_option('--model', help='Override figuring out model name by filename'),
+        make_option('--verbose', help='Emit noise'),
     )
 
     # Django 1.8+ support
     def add_arguments(self, parser):
         parser.add_argument('--model', help='Override figuring out model name by filename')
+        parser.add_argument('--verbose', help='Emit noise')
 
     def handle(self, *args, **options):
         filename = args[0]
@@ -37,9 +45,6 @@ class Command(BaseCommand):
             data = load(f)
 
         # import all entries
-
-        # import pdb; pdb.set_trace()
-
         # dependencies must be identified and synced first
         model_filter = ''.join(model_filter.lower().split(' '))
         content_types = ContentType.objects.filter(model=model_filter)
@@ -47,37 +52,12 @@ class Command(BaseCommand):
         for content_type in content_types:
             model = content_type.model_class()
             if issubclass(model, ParseModel):
-                # self.query(model, options.get('all'))
                 results = data['results']
                 self.save(model, model_filter, results)
 
-    def query(self, model, all):
-        limit = 500
-        page = 1
-        model_name = model.__name__
-        where = {}
-
-        # check to see if model.Meta.parse_table is specified
-        try:
-            model_name = model._meta.parse_table
-            print 'Querying %s (%s) on Parse...' % (model.__name__, model_name)
-        except AttributeError:
-            print 'Querying %s on Parse...' % (model_name)
-
-        while True:
-            if not all:
-                last_synced = self.psc.get_last_updated_item_from_parse(model)
-                if (last_synced):
-                    where['updatedAt'] = {'$gt': {'__type': 'Date', 'iso': last_synced}}
-
-            query = self.pc.query(model_name, where=where, order='updatedAt', limit=limit)
-            results = query['results']
-
-            self.save(model, model_name, results)
-            if len(results) < limit:
-                break
-            else:
-                page += 1
+    def verbose_print(self, thing):
+        if options.get('verbose'):
+            print thing
 
     def save(self, model, model_name, results):
         for item in results:
@@ -87,13 +67,16 @@ class Command(BaseCommand):
 
             try:
                 instance = model.objects.get(object_id=object_id)
-                print '\tUpdating Django %s.%s, last updated at %s...' % (model_name, object_id, updated_at)
+                self.verbose_print('\tUpdating Django %s.%s, last updated at %s...' % (model_name, object_id, updated_at))
             except model.DoesNotExist:
-                print '\tCreating Django %s.%s, last updated at %s...' % (model_name, object_id, updated_at)
+                self.verbose_print('\tCreating Django %s.%s, last updated at %s...' % (model_name, object_id, updated_at))
                 instance = model()
 
             for key, value in item.items():
                 snake_key = to_snake_case(key)
+
+                if key == 'authData':
+                    continue # TODO: handle facebook auth data
 
                 if type(value) != dict:
                     setattr(instance, snake_key, value)
@@ -119,6 +102,3 @@ class Command(BaseCommand):
             # saving and associating files
             for f in files:
                 f[0].save(f[1], f[2])
-
-            # self.psc.set_last_updated_item_from_parse(model, updated_at)
-            # self.psc.save()
